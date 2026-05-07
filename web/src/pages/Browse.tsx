03b23bd23da654b7e039b6b5e2640b17f8b8473a
MraK0ff <slavamarloy98@gmail.com>
2026-05-07 16:53:54 +0300

    Fix: add useEffect import and CSS types

--- web/src/pages/Browse.tsx
+++ web/src/pages/Browse.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { getAnimeList } from '../api/client';
 import { useStore } from '../store/useStore';
